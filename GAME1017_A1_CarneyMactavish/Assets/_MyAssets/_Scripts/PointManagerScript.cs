66095e6888d52dcab42f0cc955a78365a0bba97c
Mactavish-Carney <maccarney@hotmail.ca>
2025-02-20 21:51:32 -0500

    dont mind this

--- GAME1017_A1_CarneyMactavish/Assets/_MyAssets/_Scripts/PointManagerScript.cs
+++ GAME1017_A1_CarneyMactavish/Assets/_MyAssets/_Scripts/PointManagerScript.cs
@@ -19,27 +19,7 @@ public class PointManager : MonoBehaviour
     public TMP_Text highScoreLabel;
 
 
-    //private static PointManager instance = null;
-    //public static PointManager Instance
-    //{
-    //    get { return instance; }
-    //}
-    //void Awake()
-    //{
-    //    if (instance != null && instance != this)
-    //    {
-    //        Destroy(this.gameObject);
-    //        return;
-    //    }
-    //    else
-    //    {
-    //        instance = this;
-    //    }
-    //    DontDestroyOnLoad(this.gameObject);
-    //}
-
-
-    // Start is called before the first frame update
+   
     void Start()
     {
         aud = GetComponent<AudioSource>();
